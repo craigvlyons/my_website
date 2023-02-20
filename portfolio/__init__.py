acead44a784eb200e6e06847f7ac2f10d46212d7
Craig Lyons <craig@deepplanetresearch.com>
2023-02-20 16:12:26 -0500

    added gunicorn, fixed some grammer

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,57 +1,61 @@
 from flask import Flask, abort, render_template
 
-app = Flask(__name__)
 
-projects = [
-    {   
-    "name": "Flask Image server",
-    "thumb": "img/application.png",
-    "hero": "img/application.png",
-    "categories": ["Python", "Flask", "Dropzone", "HTML", "CSS"],
-    "slug": "image_server",
-    "prod": "https://github.com/craigvlyons/Flask-image-server.git"
-    },
-    {   
-    "name": "Solidworks Addin",
-    "thumb": "img/solidworks.png",
-    "hero": "img/solidworks.png",
-    "categories": ["VB.NET", "SQL", "Solidworks API"],
-    "slug": "solidworks",
-    "prod": ""
-    },
-    {   
-    "name": "Job Tracker",
-    "thumb": "img/computer.png",
-    "hero": "img/dash.png",
-    "categories": ["Python", "SQL", "Dash", "Plotly", "HTML", "CSS"],
-    "slug": "job_tracker",
-    "prod": ""
-    },
-]
+def create_app():
+    app = Flask(__name__)
 
-slug_to_project = {project["slug"]: project for project in projects }
+    projects = [
+        {   
+        "name": "Flask Image server",
+        "thumb": "img/application.png",
+        "hero": "img/application.png",
+        "categories": ["Python", "Flask", "Dropzone", "HTML", "CSS"],
+        "slug": "image_server",
+        "prod": "https://github.com/craigvlyons/Flask-image-server.git"
+        },
+        {   
+        "name": "Solidworks Addin",
+        "thumb": "img/solidworks.png",
+        "hero": "img/solidworks.png",
+        "categories": ["VB.NET", "SQL", "Solidworks API"],
+        "slug": "solidworks",
+        "prod": ""
+        },
+        {   
+        "name": "Job Tracker",
+        "thumb": "img/computer.png",
+        "hero": "img/dash.png",
+        "categories": ["Python", "SQL", "Dash", "Plotly", "HTML", "CSS"],
+        "slug": "job_tracker",
+        "prod": ""
+        },
+    ]
 
-@app.route("/")
-def home():
-    return render_template("home.html", projects=projects)
+    slug_to_project = {project["slug"]: project for project in projects }
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+    @app.route("/")
+    def home():
+        return render_template("home.html", projects=projects)
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
 
-@app.route("/project/<string:slug>")
-def project(slug):
-    if slug not in slug_to_project:
-        abort(404)
-    return render_template(
-        f"project_{slug}.html", 
-        project=slug_to_project[slug]
-        )
+    @app.route("/contact")
+    def contact():
+        return render_template("contact.html")
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template("404.html"), 404
+    @app.route("/project/<string:slug>")
+    def project(slug):
+        if slug not in slug_to_project:
+            abort(404)
+        return render_template(
+            f"project_{slug}.html", 
+            project=slug_to_project[slug]
+            )
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template("404.html"), 404
+
+    return app
